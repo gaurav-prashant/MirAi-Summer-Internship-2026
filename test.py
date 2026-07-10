fdb5e27b9ab694e17a37bfcd59ac75813c0e84f3
Prashant Kumar Gaurav <PG96082219@gmail.com>
2026-07-10 16:20:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-from google import genai
-from dotenv import load_dotenv
-import os
 
-load_dotenv(override=True)
-
-api_key = os.getenv("GEMINI_API_KEY")
-
-print("API Key =", api_key)
-
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Hello"
-)
-
-print(response.text)
